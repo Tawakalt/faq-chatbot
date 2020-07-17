97eeb7ded6fc7bab411f91ae8e35491c9c41b7dd
Taiwo <taiwo@Babatundes-MacBook-Pro.local>
2020-07-17 22:25:25 +0100

    define submit method for SalesForm

--- actions.py
+++ actions.py
@@ -4,28 +4,10 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
 from rasa_sdk.forms import FormAction
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
 
 
 class SalesForm(FormAction):
@@ -44,3 +26,12 @@ class SalesForm(FormAction):
             "company",
             "business_email",
         ]
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        dispatcher.utter_message("Thanks for getting in touch, we'll contact you soon")
+            return []
